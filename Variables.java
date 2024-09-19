22240e8ac58ba20d2d27144d8c32001d51a1a4a9
andreamsgi27 <andreamsgi27@hotmail.com>
2024-09-19 17:59:10 +0200

    declarar variables e imprimir pero no intercambiar

---
+++ Variables.java
@@ -0,0 +1,9 @@
+public class Variables {
+    public static void main (String[] args){
+        int a = 1;
+        int b = 2;
+        float f1 = 1.1f;
+        char letra = 'a';
+        System.out.println(a + " " + b + " " + f1 + " " + letra);
+    }
+}
