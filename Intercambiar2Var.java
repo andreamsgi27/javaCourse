add35ac1006f01bada3d0b2160d3919cccd8ad65
andreamsgi27 <andreamsgi27@hotmail.com>
2024-09-19 22:50:59 +0200

    codigo ej 3 dudoso en el procedimiento, funciona bien

---
+++ Intercambiar2Var.java
@@ -0,0 +1,10 @@
+public class Intercambiar2Var {
+    public static void main (String[]args){
+    int a = 2;
+    int b = a*2;
+    System.out.println(a * 2);
+    System.out.println("a es: " + a * 2 + " cuando antes era: " + a);
+    System.out.println("b es: " + b / 2 + " cuando antes era: " + b);
+    
+}
+}
