b5c46ed03642107b9a798a49f9df39e44cb4434d
Andrea Martínez Sánchez <145998162+andreamsgi27@users.noreply.github.com>
2024-09-20 14:37:33 +0200

    Create ParImpar.java

---
+++ ParImpar.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public  class ParImpar {
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Escribe un numero: ");
+        int num = scanner.nextInt();
+        if (num % 2 == 0){
+            System.out.print("El numero es par");
+        } else {
+            System.out.print("El numero es impar");
+        }
+        
+        scanner.close();
+    }
+}
