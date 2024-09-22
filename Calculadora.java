7e0a782008fa47ebaee4376253ae689ac246cb65
andreamsgi27 <andreamsgi27@hotmail.com>
2024-09-23 00:14:37 +0200

    ej 17 terminado

--- Calculadora.java
+++ Calculadora.java
@@ -7,25 +7,26 @@ public class Calculadora {
         System.out.println("Escribe 2 numeros: ");
         int num1 = scanner.nextInt();
         int num2 = scanner.nextInt();
-
+        
+        scanner.nextLine(); //sin esta linea no funcionaba lo de abajo
         System.out.println("Que operacion quieres hacer? suma, resta, multi o div?");
         String operacion = scanner.nextLine();
 
         switch (operacion) {
             case "suma":
-            System.out.println("La suma es: " + (num1+num2));
+            System.out.println("La suma es: " + (num1 + num2));
             break;
 
             case "resta":
-            System.out.println("La resta es: " + (num1-num2));
+            System.out.println("La resta es: " + (num1 - num2));
             break;
 
             case "multi":
-            System.out.println("La multiplicacion es: " + (num1*num2));
+            System.out.println("La multiplicacion es: " + (num1 * num2));
             break;
 
             case "div":
-            System.out.println("La division es: " + (num1/num2));
+            System.out.println("La division es: " + (num1 / num2));
             break;
 
             default:
