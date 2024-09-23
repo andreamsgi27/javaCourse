4302a478bc2b239a006a637e4e8df231f196b303
andreamsgi27 <andreamsgi27@hotmail.com>
2024-09-23 22:53:25 +0200

    ej. 25 patrón piramidal

--- Piramide.java
+++ Piramide.java
@@ -1,16 +1,13 @@
 public class Piramide {
     public static void main(String[] args) {
-    int cateto = 10;
 
-            for (int j = 0; j <= cateto; j++)
+            for (int vertical = 0; vertical < 10; vertical++)
             {
-                    for (int i = cateto; i <= cateto; i--)
+                System.out.println("");
+                    for (int horizontal = 0; horizontal <= vertical; horizontal++)
                     {
-                        System.out.println("");
+                        System.out.print("*");
                     }
-
-                System.out.print("*");
-                if (cateto == 0) { break; }
             }
             
         }
