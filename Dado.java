5ef83053fe8ea45ac1374ef63c1d18b1140ca472
andreamsgi27 <andreamsgi27@hotmail.com>
2024-09-23 16:22:36 +0200

    ej 13 completado. Faltaba 1 dado

--- Dado.java
+++ Dado.java
@@ -3,6 +3,7 @@ public class Dado {
     public static void main (String[] args){
         Random random = new Random();
         int randomnum = random.nextInt(1, 7);
-        System.out.print("Tu dado marca: " + randomnum);
+        int randomnum2 = random.nextInt(1, 7);
+        System.out.print("Un dado marca: " + randomnum + " y el otro: " + randomnum2);
     }
 }
