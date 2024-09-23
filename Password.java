713ed6464ca550a16431d65f38dc2ae4a0b0edf8
andreamsgi27 <andreamsgi27@hotmail.com>
2024-09-23 17:08:56 +0200

    ej 14 completo, con opcion a num de caracteres de contraseña

---
+++ Password.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Password {
+    public static void main(String[] args) {
+        Random random = new Random();
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.println("Que numero de caracteres quieres en tu contraseña?");
+        int num = scanner.nextInt();
+        char[] password = new char[num];
+        
+        for (int i = 0; i < password.length; i++){
+            if (random.nextBoolean()) {
+                password[i] = (char) ('0' + random.nextInt(10));
+            } else {
+                password[i] = (char) ('A' + random.nextInt(26));
+            }
+        }
+
+        System.out.println("Tu contraseña generada es: " + new String(password));
+    }
+}
