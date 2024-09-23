8385d26666d2bc81b7967140a4dacb9ce41ad010
andreamsgi27 <andreamsgi27@hotmail.com>
2024-09-23 12:32:24 +0200

    cambios math

---
+++ MathOperaciones.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class MathOperaciones {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Escribe un numero: ");
+        int num1 = scanner.nextInt();
+        System.out.println("Escribe otro numero: ");
+        int num2 = scanner.nextInt();
+
+        double resultadopow = Math.pow(num1, num2);
+        System.out.println(num1 + " elevado a " + num2 + " da: " + resultadopow);
+
+        double resultadosqrt1 = Math.sqrt(num1);
+        double resultadosqrt2 = Math.sqrt(num2);
+        System.out.println("La raiz cuadrada de " + num1 + " es " + resultadosqrt1 + " y la de " + num2 + " es " + resultadosqrt2);
+
+        double absoluto1 = Math.abs(num1);
+        double absoluto2 = Math.abs(num2);
+        System.out.println("El valor absoluto de " + num1 + " es " + absoluto1 + " y "
+        scanner.close();
+    }
+}
