9b4ff14771258d1fee74781a6b648d8837582317
andreamsgi27 <andreamsgi27@hotmail.com>
2024-09-23 17:15:05 +0200

    ej 19 edad para votar completo

---
+++ Votar.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class Votar {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Que edad tienes? ");
+        int edad = scanner.nextInt();
+
+        if (edad >= 18){
+            System.out.println("Puedes votar en las elecciones");
+        } else {
+            System.out.println("Aun no tienes la edad legal para votar");
+        }
+    }
+}
