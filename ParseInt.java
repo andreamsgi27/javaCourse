67a8a1140e5caa764c3de15cd17f77af6f3973ae
andreamsgi27 <andreamsgi27@hotmail.com>
2024-09-25 00:55:17 +0200

    ej 33 utilización parse int para pasar de un string (convertible a int)

---
+++ ParseInt.java
@@ -0,0 +1,7 @@
+public class ParseInt {
+    public static void main(String[] args) {
+        String palabra = "40";
+        int intPalabra = Integer.parseInt(palabra);
+        System.out.println(intPalabra + 10);
+    }
+}
