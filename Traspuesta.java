fc0f8104433773f7a4f39e2deeb3944276e03d3c
andreamsgi27 <andreamsgi27@hotmail.com>
2024-09-24 23:19:05 +0200

    ej 30 completo de 3 columnas y 2 filas del array 2d a 3 filas y 2 columnas

--- Traspuesta.java
+++ Traspuesta.java
@@ -1,5 +1,22 @@
 public class Traspuesta {
     public static void main(String[] args) {
+        int[][] bidimensional = {{1, 2, 3},{4, 5, 6}};
+        int filas = bidimensional.length;
+        int columnas = bidimensional[0].length;
+        int[][] traspuesto = new int[columnas][filas];
+
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                traspuesto[j][i] = bidimensional[i][j];
+            }
+        }
+
+        for (int i = 0; i < traspuesto.length; i++) {
+            for (int j = 0; j < traspuesto[i].length; j++) {
+                System.out.print(traspuesto[i][j] + " ");
+            }
+            System.out.println();
+        }
         
     }
 }
