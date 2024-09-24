c10fb14e3f562e06ce81b68439028181e2c5bb32
andreamsgi27 <andreamsgi27@hotmail.com>
2024-09-25 01:03:35 +0200

    ej 34 búsqueda y uso de "MAX/MIN_VALUE" para int, double y char (el cual se tiene que hacer en un int para que funcione)

---
+++ MaxMinValues.java
@@ -0,0 +1,19 @@
+public class MaxMinValues {
+    public static void main(String[] args) {
+        int maxInt = Integer.MAX_VALUE;
+        int minInt = Integer.MIN_VALUE;
+
+        double maxDouble = Double.MAX_VALUE;
+        double minDouble = Double.MIN_VALUE;
+
+        int maxChar = Character.MAX_VALUE;
+        int minChar = Character.MIN_VALUE;
+
+        System.out.println("Máximo int: " + maxInt);
+        System.out.println("Mínimo int: " + minInt);
+        System.out.println("Máximo double: " + maxDouble);
+        System.out.println("Mínimo double: " + minDouble);
+        System.out.println("Máximo char: " + maxChar);
+        System.out.println("Mínimo char: " + minChar);
+    }
+}
