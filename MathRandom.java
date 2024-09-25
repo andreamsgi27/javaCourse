dd004cd555423c80437fd38ca562f62b2fca8b36
andreamsgi27 <andreamsgi27@hotmail.com>
2024-09-25 11:04:28 +0200

    ej 12 con math random

---
+++ MathRandom.java
@@ -0,0 +1,6 @@
+public class MathRandom {
+    public static void main(String[] args) {
+        int random = (int) (Math.random() * 100);
+        System.out.println("Num aleatorio: " + random);
+    }
+}
