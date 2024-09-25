6665c576533a07876896c33938e24f2dbf416b72
andreamsgi27 <andreamsgi27@hotmail.com>
2024-09-25 09:53:16 +0200

    ej 35, array dado, añadir un numero, suma de el array actualizado

--- ListaDinamica.java
+++ ListaDinamica.java
@@ -7,24 +7,24 @@ public class ListaDinamica {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int[] listaArray = {1, 3, 5, 8};
-        ArrayList list = new ArrayList<>(Arrays.asList(listaArray));
+        Integer[] listaArray = {1, 3, 5, 8};
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(listaArray));
 
         System.out.println("Lista Original: ");
 
         for (Integer numero : listaArray) {
-            System.out.println(numero);
+            System.out.print(numero + " ");
         }
-
+        System.out.println();
         System.out.println("Añade un número: ");
         list.add(scanner.nextInt());
         
-        int suma;
-        for (int i = 0; i < listaArray.length - 1; i++){
-            suma = listaArray[i] + listaArray[i + 1];
+        int suma = 0;
+        for (int i = 0; i < list.size(); i++){
+            suma += list.get(i);
         }
         
-        System.out.println(suma);
+        System.out.println("La suma de todos los números del array ahora es: " + suma);
 
         
         scanner.close();
